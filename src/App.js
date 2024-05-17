fe64b153fe8212ed6e29893d94830ded63df4d2b
nurislom06 <nurislommominov.67@gmail.com>
2024-05-17 14:44:34 +0500

    next change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import Adminpage1 from "./components/adminpage1/adminpage1";
+// import Adminpage1 from "./components/adminpage1/adminpage1";
 import Home from "./components/home/home";
 
 function App() {
   return (
     <>
       <Home />
-      <Adminpage1 />
+      {/* <Adminpage1 /> */}
     </>
   );
 }
