e7e9865f814657ac68151c0b090eac88e68efb68
uzcoder-oo8 <sardortoxtoshov@gmail.com>
2024-05-17 14:55:25 +0500

    ad

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import Home from "./components/home/home";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Home />  
-      <Adminpage1 />
-=======
       <Home />
-      {/* <Adminpage1 /> */}
->>>>>>> 5c3100a6b62d0c076f2f6654896d5bd30a5ab8a1
+      <Adminpage1 />
+      {/* <Home /> */}
     </>
   );
 }
